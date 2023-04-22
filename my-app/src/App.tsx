0be148c77c7d352acd92e709c2caf0e0414f948f
Slipoy <slipoy.go@gmail.com>
2023-04-22 11:38:22 +0300

    create

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Player from "./components/Player";
+import './App.css';
+
+function App() {
+  return (
+      <div style={{width: '100%', height: '100%'}}>
+        <Player />
+      </div>
+  );
+}
+
+export default App;
